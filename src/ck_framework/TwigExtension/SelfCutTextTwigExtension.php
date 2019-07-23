c4796d081d63715d2222df4577d5613d746e3e58
Jonathan <jonathan.gleyze@gmail.com>
2019-07-23 21:04:01 +0200

    [add] self Warp Todo

--- src/ck_framework/TwigExtension/SelfCutTextTwigExtension.php
+++ src/ck_framework/TwigExtension/SelfCutTextTwigExtension.php
@@ -12,6 +12,7 @@ class SelfCutTextTwigExtension extends AbstractExtension
     {
         return [
             new TwigFunction("SelfCut", [$this, 'SelfCutString']),
+            new TwigFunction("SelfWarp", [$this, 'SelfWrappingString']),
         ];
     }
 
@@ -23,9 +24,32 @@ class SelfCutTextTwigExtension extends AbstractExtension
      * @param string $end
      * @return string
      */
-    public function SelfCutString(string $string, int $start_char, string $length, string $end = "..."){
+    public function SelfCutString(string $string, int $start_char, string $length, string $end = "..."): string {
         if( strlen( $string ) <= $length ) return $string;
         $str = mb_substr( $string, $start_char, $length - strlen( $end ) + 1, 'UTF-8');
         return substr( $str, 0, strrpos( $str,' ') ). ' ' .$end;
+    }
+
+    /**
+     * return self cut end string
+     * @param string $string
+     * @param int $start_char
+     * @param int $_cut_count
+     * @return string
+     */
+    public function SelfWrappingString(string $string, int $start_char, int $_cut_count){
+        $stringCount = strlen($string);
+
+        $number = ceil($stringCount / $_cut_count);
+        $cut = [];
+        for ($i = 1; $i <= $stringCount - 1; $i++) {
+            $c = 0;
+            dump($i);
+
+            $c++;
+        }
+        $string = substr($string, $start_char, $_cut_count);
+        dump($string);
+        return $string;
     }
 }

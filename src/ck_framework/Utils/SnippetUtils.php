1db847c584a5d8d61a1f24a033600603753e8898
Jonathan <jonathan.gleyze@gmail.com>
2019-08-05 12:05:16 +0200

    [add] add ArrayArgsToHtml function

--- src/ck_framework/Utils/SnippetUtils.php
+++ src/ck_framework/Utils/SnippetUtils.php
@@ -18,7 +18,6 @@ class SnippetUtils
         }
     }
 
-
     /**
      * check if string contain specific char
      * @param string $catch
@@ -33,12 +32,43 @@ class SnippetUtils
         }
     }
 
+    /**
+     * catch text after specific character
+     * @param string $catch
+     * @param string $char
+     * @return bool|string
+     */
     static function GetTextAfterChar(string $catch, string $char){
         return substr($char, strpos($char, $catch) +1 );
     }
 
+    /**
+     * catch text before specific character
+     * @param string $catch
+     * @param string $char
+     * @return mixed
+     */
     static function GetTextBeforeChar(string $catch, string $char){
         $arr = explode($catch, $char);
         return $arr[0];
+    }
+
+    /**
+     * return string formatted for html
+     * @param array $args
+     * @return string
+     */
+    static function ArrayArgsToHtml(array $args): string{
+        return implode(' ', array_map(
+            function ($v, $k) {
+                if(is_array($v)){
+                    return $k.'[]='.implode('&'.$k.'[]=', $v);
+                }else{
+                    return $k.'="'.$v. '"';
+                }
+            },
+            $args,
+            array_keys($args)
+        ));
     }
 }

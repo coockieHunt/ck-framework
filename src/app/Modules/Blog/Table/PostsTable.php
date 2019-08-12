320244e668acd315666e3d864858ed165aca1584
Jonathan <jonathan.gleyze@gmail.com>
2019-08-12 17:06:22 +0200

    [add] table use simple function

--- src/app/Modules/Blog/Table/PostsTable.php
+++ src/app/Modules/Blog/Table/PostsTable.php
@@ -5,32 +5,64 @@ namespace app\Modules\Blog\Table;
 
 
 use app\Modules\Blog\Entity\PostsEntity;
-use DateTime;
+use ck_framework\Database\Table;
 use PDO;
 
-class PostsTable
+class PostsTable extends Table
 {
-    /**
-     * @var PDO
-     */
-    private $PDO;
+    protected $tableName = "posts";
+    protected $entity = PostsEntity::class;
 
-    public function __construct(PDO $PDO)
-    {
-        $this->PDO = $PDO;
+
+    /**
+     * find post by slug
+     * @param string $slug (slug post ex : this-slug)
+     * @return mixed
+     */
+    public function FindBySlug(string $slug){
+        return $this->FindByValue('slug', $slug, PDO::PARAM_STR);
     }
 
+    /**
+     * find post by slug
+     * @param int $id (id post ex : 1)
+     * @return mixed
+     */
+    public function FindById(int $id){
+        return $this->FindByValue('id', $id, PDO::PARAM_STR);
+    }
+
+    /**
+     * find between range post
+     * @param int $limit (limit post ex : 1)
+     * @param int $offset (offset post  ex: 10)
+     * @return array
+     */
     public function FindResultLimit(int $limit, int $offset) :array{
+        return $this->FindByLimit($limit, $offset, 'create_at DESC');;
+    }
+
+
+    public function UpdatePost(int $id, string $name, string $content, string $slug, bool $active){
         $request = $this->PDO
-            ->prepare('SELECT * FROM posts ORDER BY posts.create_at DESC LIMIT :limit, :offset');
-        $request->bindValue(':limit', $limit, PDO::PARAM_INT);
-        $request->bindValue(':offset', $offset, PDO::PARAM_INT);
-
+            ->prepare('UPDATE posts SET name = :name, content = :content, slug = :slug , active = :active, update_at = NOW() WHERE posts.id = :id');
+        $request->bindValue(':name', $name, PDO::PARAM_STR);
+        $request->bindValue(':content', $content, PDO::PARAM_STR);
+        $request->bindValue(':slug', $slug, PDO::PARAM_STR);
+        $request->bindValue(':id', $id, PDO::PARAM_STR);
+        $request->bindValue(':active', $active, PDO::PARAM_BOOL);
         $request->execute();
-        $request->setFetchMode(PDO::FETCH_CLASS, PostsEntity::class);
-        $posts = $request->fetchAll();
+    }
 
-        return $posts;
+    public function NewPost($name, $content, $slug, $active)
+    {
+        $request = $this->PDO
+            ->prepare('INSERT INTO posts (name, content, slug, active,create_at, update_at) VALUES (:name , :content, :slug, :active, NOW(), NOW())');
+        $request->bindValue(':name', $name, PDO::PARAM_STR);
+        $request->bindValue(':content', $content, PDO::PARAM_STR);
+        $request->bindValue(':slug', $slug, PDO::PARAM_STR);
+        $request->bindValue(':active', $active, PDO::PARAM_BOOL);
+        $request->execute();
     }
 
     public function FindResultLimitFilter(string $name, string $slug, string $content, int $limit, int $offset) :array{
@@ -82,76 +114,5 @@ class PostsTable
             ->query($query)
             ->fetch();
         return $posts;
-    }
-
-    public function FindAll() :array{
-        $request = $this->PDO
-            ->prepare('SELECT * FROM posts');
-
-        $request->execute();
-        $request->setFetchMode(PDO::FETCH_CLASS, PostsEntity::class);
-        $posts = $request->fetchAll();
-
-        return $posts;
-    }
-
-    public function FindBySlug(string $slug){
-        $request = $this->PDO
-            ->prepare('SELECT * FROM posts WHERE posts.slug = :slug');
-        $request->bindValue(':slug', $slug, PDO::PARAM_STR);
-        $request->execute();
-        $request->setFetchMode(PDO::FETCH_CLASS, PostsEntity::class);
-        $post = $request->fetch();
-
-        return $post;
-    }
-
-    public function FindById(int $id){
-        $request = $this->PDO
-            ->prepare('SELECT * FROM posts WHERE posts.id = :id');
-        $request->bindValue(':id', $id, PDO::PARAM_STR);
-        $request->execute();
-        $request->setFetchMode(PDO::FETCH_CLASS, PostsEntity::class);
-        $post = $request->fetch();
-
-        return $post;
-    }
-
-    public function CountAll()
-    {
-        $posts = $this->PDO
-            ->query('SELECT COUNT(*) FROM posts')
-            ->fetch();
-        return $posts;
-    }
-
-    public function UpdatePost(int $id, string $name, string $content, string $slug,bool $active){
-        $request = $this->PDO
-            ->prepare('UPDATE posts SET name = :name, content = :content, slug = :slug , active = :active, update_at = NOW() WHERE posts.id = :id');
-        $request->bindValue(':name', $name, PDO::PARAM_STR);
-        $request->bindValue(':content', $content, PDO::PARAM_STR);
-        $request->bindValue(':slug', $slug, PDO::PARAM_STR);
-        $request->bindValue(':id', $id, PDO::PARAM_STR);
-        $request->bindValue(':active', $active, PDO::PARAM_BOOL);
-        $request->execute();
-    }
-
-    public function NewPost($name, $content, $slug, $active)
-    {
-        $request = $this->PDO
-            ->prepare('INSERT INTO posts (name, content, slug, active,create_at, update_at) VALUES (:name , :content, :slug, :active, NOW(), NOW())');
-        $request->bindValue(':name', $name, PDO::PARAM_STR);
-        $request->bindValue(':content', $content, PDO::PARAM_STR);
-        $request->bindValue(':slug', $slug, PDO::PARAM_STR);
-        $request->bindValue(':active', $active, PDO::PARAM_BOOL);
-        $request->execute();
-    }
-
-    public function DeleteById($id)
-    {
-        $request = $this->PDO
-            ->prepare('DELETE FROM posts WHERE posts.id = :id ');
-        $request->bindValue(':id', $id, PDO::PARAM_STR);
-        $request->execute();
     }
 }
---
+++ src/ck_framework/Database/Table.php
@@ -0,0 +1,120 @@
+<?php
+
+
+namespace ck_framework\Database;
+
+use app\Modules\Blog\Entity\PostsEntity;
+use ck_framework\Utils\SnippetUtils;
+use PDO;
+
+class Table
+{
+    /**
+     * @var PDO
+     */
+    protected $PDO;
+
+    /**
+     * name table name
+     * @var string
+     */
+    protected $tableName;
+
+    /**
+     * entity in table
+     * @var string null
+     */
+    protected $entity;
+
+    public function __construct(PDO $PDO)
+    {
+        $this->PDO = $PDO;
+    }
+
+    /**
+     * find all element
+     * @return array
+     */
+    public function FindAll() :array{
+        $request = $this->PDO
+            ->prepare('SELECT * FROM ' . $this->tableName);
+        if ($this->entity){$request->setFetchMode(PDO::FETCH_CLASS, $this->entity);}
+        $request->execute();
+        $response = $request->fetchAll();
+
+        return $response;
+    }
+
+    /**
+     * count all element table
+     * @return mixed
+     */
+    public function CountAll()
+    {
+        $response = $this->PDO
+            ->query('SELECT COUNT(*) FROM ' . $this->tableName)
+            ->fetch();
+        return $response;
+    }
+
+    /**
+     * Deleter entry by id
+     * @param $id
+     */
+    public function DeleteById(int $id):void
+    {
+        $request = $this->PDO
+            ->prepare('DELETE FROM '.$this->tableName.' WHERE '.$this->tableName.'.id = :id ');
+        $request->bindValue(':id', $id, PDO::PARAM_INT);
+        $request->execute();
+    }
+
+    /**
+     * Find entry by value
+     * @param string $where
+     * @param $value
+     * @param $data_type
+     * @return mixed
+     */
+    public function FindByValue(string $where, $value, $data_type){
+        $BuildWERE = $this->tableName . '.'. $where . ' = :' . $where;
+        $request = $this->PDO
+            ->prepare('SELECT * FROM '.$this->tableName.' WHERE ' . $BuildWERE);
+        $request->bindValue(':' . $where, $value, $data_type);
+        $request->execute();
+        if ($this->entity){$request->setFetchMode(PDO::FETCH_CLASS, $this->entity);}
+        $response = $request->fetch();
+
+        return $response;
+    }
+
+    /**
+     * find entry between two values
+     * @param int $limit
+     * @param int $offset
+     * @param string $order
+     * @return array
+     */
+    public function FindByLimit(int $limit, int $offset, string $order) :array{
+        $BuildOrder = ' ORDER BY '. $this->tableName . '.' . $order;
+        $request = $this->PDO
+            ->prepare('SELECT * FROM '. $this->tableName . $BuildOrder .' LIMIT :limit, :offset');
+        $request->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $request->bindValue(':offset', $offset, PDO::PARAM_INT);
+
+        $request->execute();
+        if ($this->entity){$request->setFetchMode(PDO::FETCH_CLASS, $this->entity);}
+        $response = $request->fetchAll();
+
+        return $response;
+    }
+
+    /**
+     * get Entity for child class
+     * @return string
+     */
+    public function getEntity(): string
+    {
+        return $this->entity;
+    }
+}

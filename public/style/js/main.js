2618a671ddb7392ee18d0647d35257f8c71d53ac
Jonathan <jonathan.gleyze@gmail.com>
2019-08-14 15:12:09 +0200

    [up] min file

--- public/style/js/main.js
+++ public/style/js/main.js
@@ -1,15 +1,2 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scroll-to-top").style.display = "block";
-    } else {
-        document.getElementById("scroll-to-top").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    $("html, body").animate({scrollTop : 0}, 1500);
-}
+window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>20||document.documentElement.scrollTop>20){document.getElementById("scroll-to-top").style.display="block"}else{document.getElementById("scroll-to-top").style.display="none"}}
+function topFunction(){$("html, body").animate({scrollTop:0},1500)}
